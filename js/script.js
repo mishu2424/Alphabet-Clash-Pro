6b6e96e56ef03d4dd10032ceb8298e0f66659a0c
Apurbo dey <apurbomishu24@gmail.com>
2025-01-11 14:19:39 -0500

    some more functions have been added

--- js/script.js
+++ js/script.js
@@ -3,44 +3,33 @@ document.querySelector('#playNowBtn').addEventListener("click",()=>{
     showElementById('#menuPage');
     const getAlphabet=play();
     setValue('#highlighted-alphabet',getAlphabet);
+    setBg(getAlphabet.toLowerCase());
 })
-
+document.addEventListener('keyup',(e)=>{
+    // if(e.key=='Escape'){
+    //     const getVal=getValue('#highlighted-alphabet');
+    //     removeBg(getVal.toLowerCase())
+    //     showElementById('#homePage');
+    //     hideElementById('#menuPage');
+    // }
+    if(e.key==='Escape'){
+        showElementById('#scorePage');
+        hideElementById('#menuPage');
+    }
+    else if('abcdefghijklmnopqrstuvwxyz'.includes(e.key)){
+        mainGame(e.key);
+    }
+    else{
+        showElementById('#alert-section');
+        setTimeout(()=>{
+            hideElementById('#alert-section');
+        },1000)
+    }
+    // mainGame(e.key);
+})
 document.querySelector('#keyboard').addEventListener("click",(e)=>{
     const typedWord=choosenWord(e);
-    const getAlphabet=getValue('#highlighted-alphabet');
-    let currentValue=getValue('#totalScore');
-    let convertedValue=parseInt(currentValue);
-    let lifeLine=getValue('#lifeLine');
-    let convertedLifeLine=parseInt(lifeLine);
-    if(convertedLifeLine!==0){
-        if(typedWord!==undefined){
-            if (getAlphabet===typedWord.toUpperCase()){
-                convertedValue++;
-                setValue('#totalScore',convertedValue);
-                if(convertedValue===5){
-                    setValue('#totalScore',convertedValue);
-                    setTimeout(()=>{
-                        showElementById('#scorePage');
-                        hideElementById('#menuPage');
-                    },200)
-                    setValue('#finalScore',convertedValue);
-                    return 'winner';
-                }
-                const getAlphabet=play();
-                setValue('#highlighted-alphabet',getAlphabet)
-            }
-            else{
-                convertedLifeLine--;
-                setValue('#lifeLine',convertedLifeLine);
-                setValue('#finalScore',convertedValue);
-            }
-        }
-        }
-    
-    if (convertedLifeLine===0){
-        hideElementById('#menuPage');
-        showElementById('#scorePage');
-    }
+    mainGame(typedWord);
 })
 
 document.querySelector('#playAgainBtn').addEventListener("click",()=>{
@@ -49,6 +38,24 @@ document.querySelector('#playAgainBtn').addEventListener("click",()=>{
     setValue('#totalScore',0);
     setValue('#finalScore',0);
     setValue('#lifeLine',2);
+    const getVal=getValue('#highlighted-alphabet');
+    removeBg(getVal.toLowerCase());
     const getAlphabet=play();
     setValue('#highlighted-alphabet',getAlphabet)
+    setBg(getAlphabet.toLowerCase());
+})
+
+document.querySelector('#keyboard').addEventListener("mouseover",(e)=>{
+    if(e.target.closest('.kbd')){
+        e.target.classList.add('bg-purple-500','text-white');
+    }
+})
+document.querySelector('#keyboard').addEventListener("mouseout",(e)=>{
+    if(e.target.closest('.kbd')){
+        e.target.classList.remove('bg-purple-500','text-white');
+    }
+})
+
+document.addEventListener('keyup',(e)=>{
+    console.log(e.key)
 })
--- js/utility.js
+++ js/utility.js
@@ -22,6 +22,63 @@ function play(){
 
 function choosenWord(e){
     if(e.target.closest('.kbd')){
+        e.target.classList.add('bg-amber-500');
+        setTimeout(()=>{
+            e.target.classList.remove('bg-amber-500');
+        },400)
         return e.target.innerText;
+    }
+}
+function setBg(elementId){
+    document.getElementById(elementId).classList.add('bg-amber-600');
+    console.log(document.getElementById(elementId).classList)
+}
+function removeBg(elementId){
+    console.log(elementId)
+    console.log(document.getElementById(elementId).classList)
+    document.getElementById(elementId).classList.remove('bg-amber-600');
+    console.log(document.getElementById(elementId).classList)
+}
+
+function mainGame(typedWord){
+    const getSelectedAlphabet=getValue('#highlighted-alphabet');
+    let currentValue=getValue('#totalScore');
+    let convertedValue=parseInt(currentValue);
+    let lifeLine=getValue('#lifeLine');
+    let convertedLifeLine=parseInt(lifeLine);
+    if(convertedLifeLine!==0){
+        if(typedWord!==undefined){
+            if (getSelectedAlphabet.toLowerCase()===typedWord){
+                removeBg(typedWord);
+                convertedValue++;
+                setValue('#totalScore',convertedValue);
+                if(convertedValue===5){
+                    setValue('#totalScore',convertedValue);
+                    setTimeout(()=>{
+                        showElementById('#scorePage');
+                        hideElementById('#menuPage');
+                    },200)
+                    document.getElementById('comment').innerText="Great Game!"
+                    // return 'winner';
+                }
+                if(convertedValue<5){
+                    const getAlphabet=play();
+                    setValue('#highlighted-alphabet',getAlphabet);
+                    setBg(getAlphabet.toLowerCase());
+                }
+                setValue('#finalScore',convertedValue);
+            }
+            else{
+                convertedLifeLine--;
+                setValue('#lifeLine',convertedLifeLine);
+                setValue('#finalScore',convertedValue);
+            }
+        }
+        }
+    
+    if (convertedLifeLine===0){
+        hideElementById('#menuPage');
+        showElementById('#scorePage');
+        document.getElementById('comment').innerText="Practice will make you Pro!"
     }
 }
